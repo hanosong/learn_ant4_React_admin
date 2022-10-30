578993ab9a5c730d716233f6a86c212490fb8b96
hanosong <838519800@qq.com>
2022-10-30 23:36:55 +0800

    feat: 安装antd4

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from "react";
+import DynamicForm from "./cpns/动态表单";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = memo(() => {
+  return <DynamicForm />;
+});
 
 export default App;
---
+++ react-demo/src/cpns/动态表单/index.jsx
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+// 动态表单组件构造
+const DynamicForm = memo(() => {
+  return <div>DynamicForm</div>;
+});
+
+export default DynamicForm;
